84c2ef2b0b846d057d642b2256ad7b3399857b63
SRIRAM6369791334 <94049113+SRIRAM6369791334@users.noreply.github.com>
2024-07-18 16:35:56 +0530

    Add files via upload

---
+++ Mern evaluation/scripts.js
@@ -0,0 +1,6 @@
+<script>
+    document.getElementById('loginForm').addEventListener('submit', function(event) {
+        event.preventDefault(); 
+        window.location.href = 'booking.htnl';  
+    });
+</script>
